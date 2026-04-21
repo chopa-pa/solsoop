d91cd2c1467faea93964e81a6737c085faf9f922
쵸파파 <chopa_lee@chyopapaui-MacBookAir.local>
2026-04-21 22:33:13 +0900

    PWA 홈 화면 추가: 아이콘, 설치 배너, iOS 안내

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'solsoop-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'solsoop-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
